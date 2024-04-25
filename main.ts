4d867395089ec883f599e0422151aa97b3dd358b
hit00-ling <167969614+hit00-ling@users.noreply.github.com>
2024-04-25 21:49:34 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
