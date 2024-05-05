15237db5a2a68e0dd35f060ec73db9dc3e453658
hit00-ling <167969614+hit00-ling@users.noreply.github.com>
2024-05-05 19:59:30 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,105 +1,36 @@
-def on_gesture_shake():
-    music.stop_all_sounds()
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-
 def on_button_pressed_ab():
     basic.show_icon(IconNames.HAPPY)
-    music.play(music.string_playable("E E G A C5 C5 A G ", 120),
-        music.PlaybackMode.UNTIL_DONE)
-    music.play(music.string_playable("G A G - - - - - ", 120),
-        music.PlaybackMode.UNTIL_DONE)
     for 值 in list2:
         basic.show_string("" + (值))
+    for index in range(5):
+        music.play(music.string_playable("E E G A C5 C5 A G ", 120),
+            music.PlaybackMode.UNTIL_DONE)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def on_ir_button_pressed():
+def doSomething(array: List[any], image: Image, sprite: game.LedSprite, array2: List[any], 数字: number, 布尔值: bool, 文本: str):
     pass
-makerbit.on_ir_button(IrButton.ANY, IrButtonAction.PRESSED, on_ir_button_pressed)
 
-def on_gesture_free_fall():
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
+
+def on_gesture_three_g():
     basic.show_leds("""
+        . . # . .
+        . . # . .
         # # # # #
-        # # # # .
-        # # # . .
-        # # . . .
-        # . . . .
+        . . # . .
+        . . # . .
         """)
-input.on_gesture(Gesture.FREE_FALL, on_gesture_free_fall)
+input.on_gesture(Gesture.THREE_G, on_gesture_three_g)
 
-def on_ir_datagram():
-    if makerbit.ir_button() == 162:
-        music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
-            music.PlaybackMode.IN_BACKGROUND)
-        basic.show_leds("""
-            . . # . .
-            . . # . .
-            # # # # #
-            . . # . .
-            . . # . .
-            """)
-    elif makerbit.ir_button() == 98:
-        music.play(music.tone_playable(294, music.beat(BeatFraction.HALF)),
-            music.PlaybackMode.IN_BACKGROUND)
-        basic.show_leds("""
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            """)
-    elif makerbit.ir_button() == 226:
-        music.play(music.tone_playable(330, music.beat(BeatFraction.HALF)),
-            music.PlaybackMode.IN_BACKGROUND)
-        basic.show_leds("""
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            """)
-    elif makerbit.ir_button() == 34:
-        music.play(music.tone_playable(349, music.beat(BeatFraction.HALF)),
-            music.PlaybackMode.IN_BACKGROUND)
-        basic.show_leds("""
-            . . # . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . # . .
-            """)
-    elif makerbit.ir_button() == 2:
-        music.play(music.tone_playable(392, music.beat(BeatFraction.HALF)),
-            music.PlaybackMode.IN_BACKGROUND)
-        basic.show_leds("""
-            . . . . .
-            # # # # #
-            . . . . .
-            # # # # #
-            . . . . .
-            """)
-    else:
-        music.play(music.tone_playable(988, music.beat(BeatFraction.QUARTER)),
-            music.PlaybackMode.UNTIL_DONE)
-        music.play(music.tone_playable(523, music.beat(BeatFraction.QUARTER)),
-            music.PlaybackMode.IN_BACKGROUND)
-        basic.show_leds("""
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            """)
-makerbit.on_ir_datagram(on_ir_datagram)
-
-"""
-
-"""
 list2: List[str] = []
+images.create_big_image("""
+    . # . . . . # # . .
+    # # . . . # . . # .
+    . # . . . . . # . .
+    . # . # . . # . . .
+    . # . # . # # # # .
+    """).scroll_image(1, 200)
 music.set_volume(255)
-list2 = ["a", "b", "c", "d", "e"]
-basic.show_icon(IconNames.HEART)
-makerbit.connect_ir_receiver(DigitalPin.P16, IrProtocol.NEC)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+list2 = ["a", "b", "c"]
--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let list2: number[] = []
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Happy)
     for (let 值 of list2) {
@@ -8,69 +7,11 @@ input.onButtonPressed(Button.AB, function () {
         music.play(music.stringPlayable("E E G A C5 C5 A G ", 120), music.PlaybackMode.UntilDone)
     }
 })
-makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
+function doSomething (array: any[], image: Image, sprite: game.LedSprite, array2: any[], 数字: number, 布尔值: boolean, 文本: string) {
 	
-})
+}
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
-})
-makerbit.onIrDatagram(function () {
-    if (makerbit.irButton() == 162) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # # # # #
-            . . # . .
-            . . # . .
-            `)
-    } else if (makerbit.irButton() == 98) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
-    } else if (makerbit.irButton() == 226) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-    } else if (makerbit.irButton() == 34) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        basic.showLeds(`
-            . . # . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . # . .
-            `)
-    } else if (makerbit.irButton() == 2) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
-    } else {
-        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    }
 })
 input.onGesture(Gesture.ThreeG, function () {
     basic.showLeds(`
@@ -81,23 +22,33 @@ input.onGesture(Gesture.ThreeG, function () {
         . . # . .
         `)
 })
-input.onGesture(Gesture.TiltRight, function () {
-    music.stopAllSounds()
-})
+let text_list = 0
+let list2: string[] = []
 images.createBigImage(`
     . # . . . . # # . .
     # # . . . # . . # .
     . # . . . . . # . .
     . # . # . . # . . .
     . # . # . # # # # .
-    `).scrollImage(6, 200)
+    `).scrollImage(1, 200)
 music.setVolume(255)
+list2 = ["a", "b", "c"]
 basic.forever(function () {
-	
-})
-basic.forever(function () {
-	
-})
-basic.forever(function () {
-	
+    if (Math.randomBoolean()) {
+        makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.Keyestudio)
+    } else if (Math.randomBoolean()) {
+        if (true) {
+            radio.sendValue("name", 0)
+        } else {
+            radio.sendValue("name", 0)
+        }
+    } else {
+        if (true) {
+            for (let index = 0; index < 5 * 10; index++) {
+                led.stopAnimation()
+            }
+        } else {
+            text_list += 1
+        }
+    }
 })
